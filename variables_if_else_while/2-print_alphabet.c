e5209bd601ee6a5b2d122fd4d3badb8e02f75e4d
Alexdoescode123 <alexanderg.diaz.mejia@gmail.com>
2024-02-23 13:58:39 -0400

    Updated

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	n = 97;
+	for (; n <= 122;)
+	{
+		putchar(n++);
+	}
+	return (0);
+}
+
