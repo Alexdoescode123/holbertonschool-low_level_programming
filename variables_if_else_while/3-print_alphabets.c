7173c0227619d971e4a7c2c3f0b050b7fd33c5e8
Alexdoescode123 <alexanderg.diaz.mejia@gmail.com>
2024-02-25 13:40:19 -0400

    Updated

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
+
 /**
- * main- Entry point
+ * main - Entry point
  * Return: Always 0 (Success)
-*/
+ */
 int main(void)
 {
 int n;
 int N;
-N = 64;
-n = 97;
-for (; n <= 122;)
+n = 97; // Lowercase 'a'
+N = 65; // Uppercase 'A'
+while (n <= 122)
 {
 putchar(n++);
 }
-for (; N <= 90;)
+while (N <= 90)
 {
 putchar(N++);
 }
